378832853dfcb5ef0e307ab7fea74adac7510dd9
Rainer Nsa <woldreamz@gmail.com>
2024-12-10 03:28:02 +0100

    Code Refactoring

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,9 +5,10 @@ interface ButtonProps {
   children: React.ReactNode;
   px?: string;
   white?: boolean;
+  variant?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ className, href, onClick, children, px, white }) => {
+const Button: React.FC<ButtonProps> = ({ className, href, onClick, children, px, white, variant }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
